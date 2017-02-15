ed139a5537f37beb406075cc531f3a16ffa60e0d
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-02-15 10:47:51 -0500

    home page lists posts and comments, sign-in brings user to profile page with info

--- main.rb
+++ main.rb
@@ -7,3 +7,24 @@ require "faker"
 set :database, "sqlite3:test.sqlite3"
 enable :sessions
 
+get '/' do
+  @users = User.all
+  @posts = Post.all
+  erb :index
+end
+
+post '/sign-in' do
+  @user = User.where(username: params[:username]).first
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect "/profile/#{@user.id}"
+  else
+    flash[:notice] = "Sorry, your login didn't work. Try again"
+    redirect '/'
+  end
+end
+
+get '/profile/:id' do
+  @user = User.find(params[:id])
+  erb :profile
+end
