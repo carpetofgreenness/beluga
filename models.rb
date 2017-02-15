5d68691b1e75d66f8b200a6146e98cf39883cfa6
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-02-15 10:08:25 -0500

    created comments table and added some comments to db

--- models.rb
+++ models.rb
@@ -1,12 +1,19 @@
 class User < ActiveRecord::Base
 	has_many :posts
+	has_many :comments
 end
 
 class Post < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :tag
+	has_many :comments
 end
 
 class Tag < ActiveRecord::Base
 	has_many :posts
+end
+
+class Comment < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :post
 end
